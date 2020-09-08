4f2fdccd0987ed82c1b2a8e5c0f1f9c9d6f420fa
Aldert Vaandering <aldert.vaandering@digib.com>
2020-09-08 03:34:23 +0300

    Created button component

---
+++ source/ContentScript/Components/Button/Button.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { LoadingBars } from '../Spinner/LoadingBars'
+
+interface ButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+  disabled?: boolean
+  loading?: boolean
+}
+
+export const StyledButton = styled.div<ButtonProps>`
+  height: 34px;
+  padding: 0 10px;
+  border-radius: 3px;
+
+  background: ${props => props.theme.color.secondary};
+  color: ${props => props.theme.color.white};
+
+  display: grid;
+  place-content: center;
+
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  ${props =>
+    props.disabled && {
+      cursor: 'not-allowed',
+      opacity: 0.6,
+    }}
+
+  ${props =>
+    props.loading && {
+      cursor: 'wait',
+    }}
+`
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  loading,
+  ...props
+}) => (
+  <StyledButton {...props} loading={loading}>
+    {loading ? <LoadingBars size={15} /> : children}
+  </StyledButton>
+)
