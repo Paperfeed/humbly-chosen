5b96ab831afa8abd02818dec9c91a32afbe394ef
Aldert Vaandering <aldert.vaandering@digib.com>
2020-09-11 03:00:27 +0300

    Added local storage functionality

--- source/lib/local-storage.ts
+++ source/lib/local-storage.ts
@@ -1,5 +1,6 @@
 import { browser } from 'webextension-polyfill-ts'
 
+import { GameInfoResponse } from '../messages/content-messages'
 import { StorageKey } from './enums'
 
 export interface StoredOptions {
@@ -7,14 +8,31 @@ export interface StoredOptions {
   username: string
 }
 
-export function loadFromLocalStorage(key: StorageKey): unknown {
-  return localStorage.getItem(key)
+export type StoredHumbleChoiceData = Record<string, GameInfoResponse[]>
+
+type StoredRecords<T extends StorageKey> = Record<T, object>
+
+export interface StoredData extends StoredRecords<StorageKey> {
+  [StorageKey.Options]: StoredOptions
+  [StorageKey.HumbleChoiceData]: StoredHumbleChoiceData
 }
 
-export const getFromStorage = async <T>(key: StorageKey): Promise<T> => {
+export const getFromStorage = async <T extends StorageKey>(
+  key: T,
+): Promise<StoredData[T]> => {
   const storedItem = await browser.storage.sync.get(key)
   return storedItem[key]
 }
 
 export const saveToStorage = async <T>(item: T) =>
   await browser.storage.sync.set(item)
+
+export const getFromLocalStorage = async <T extends StorageKey>(
+  key: T,
+): Promise<StoredData[T]> => {
+  const storedItem = await browser.storage.local.get(key)
+  return storedItem[key]
+}
+
+export const saveToLocalStorage = async <T>(item: T) =>
+  await browser.storage.local.set(item)
