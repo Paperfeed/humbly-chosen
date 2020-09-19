c46887a8981a31468b92aea6562677b7d75e047a
Aldert Vaandering <aldert.vaandering@digib.com>
2020-09-19 22:32:04 +0300

    Added new global constants and enums

--- source/lib/enums.ts
+++ source/lib/enums.ts
@@ -1,5 +1,15 @@
 export enum StorageKey {
+  AppList = 'app_list',
   HumbleChoiceData = 'humble_choice_data',
   Options = 'options',
+  OwnedGames = 'owned_games',
   SteamGames = 'steam_games',
 }
+
+export enum PageType {
+  HumbleChoice = 'humbleChoice',
+  HumbleGameBundle = 'humbleGameBundle',
+}
+
+export const USE_CACHING = true
+export const ENABLE_DEBUG = true
