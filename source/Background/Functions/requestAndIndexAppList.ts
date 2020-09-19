efdc32a4b10440833c553d8bc47fcb9bf26ee574
Aldert Vaandering <aldert.vaandering@digib.com>
2020-09-19 22:13:59 +0300

    Added rudimentary caching

--- source/Background/Functions/requestAndIndexAppList.ts
+++ source/Background/Functions/requestAndIndexAppList.ts
@@ -1,6 +1,6 @@
 import { indexSteamAppList } from '../../lib/db'
 import { Debug, Verbosity } from '../../lib/debug'
-import { StorageKey } from '../../lib/enums'
+import { StorageKey, USE_CACHING } from '../../lib/enums'
 import {
   getFromLocalStorage,
   saveToLocalStorage,
@@ -9,14 +9,37 @@ import { requestFromSteam, SteamEndpoint } from '../../lib/request'
 
 export async function requestAndIndexApplist(apiUrl: string) {
   Debug.log(Verbosity.INFO, `Requesting steamAppList from ${apiUrl}`)
-  try {
-    // TODO refactor
-    let steamAppList
-    const temp = await getFromLocalStorage(StorageKey.AppList)
-    if (temp) {
-      steamAppList = temp.data
-    } else {
-      steamAppList = await requestFromSteam(apiUrl, SteamEndpoint.GetAppList)
+
+  const fromStorage = await getFromLocalStorage(StorageKey.AppList)
+  if (
+    USE_CACHING &&
+    fromStorage &&
+    fromStorage?.data?.applist?.apps?.length &&
+    Date.now() - fromStorage.timestamp <= 1000 * 60 * 30
+  ) {
+    Debug.log(
+      Verbosity.INFO,
+      `Retrieved AppList from storage, containing ${
+        fromStorage.data.applist.apps.length
+      } records, from ${new Date(fromStorage.timestamp).toTimeString()}`,
+    )
+    Debug.log(Verbosity.LOG, 'Cached data is less than 30 minutes old')
+    await indexSteamAppList(fromStorage.data)
+  } else {
+    try {
+      Debug.log(
+        Verbosity.LOG,
+        'Cached data is more than 30 minutes old or non-existent, calling API',
+      )
+
+      const steamAppList = await requestFromSteam(
+        apiUrl,
+        SteamEndpoint.GetAppList,
+      )
+
+      if (!steamAppList.applist.apps.length) {
+        throw new Error('Steam AppList returned 0 results')
+      }
 
       await saveToLocalStorage({
         [StorageKey.AppList]: {
@@ -24,15 +47,11 @@ export async function requestAndIndexApplist(apiUrl: string) {
           timestamp: Date.now(),
         },
       })
-    }
 
-    if (!steamAppList.applist.apps.length) {
-      throw new Error('Steam AppList returned 0 results')
+      await indexSteamAppList(steamAppList)
+    } catch (e) {
+      Debug.log(Verbosity.WARN, `Failed to retrieve applist from ${apiUrl}`)
+      throw e
     }
-
-    await indexSteamAppList(steamAppList)
-  } catch (e) {
-    Debug.log(Verbosity.WARN, `Failed to retrieve applist from ${apiUrl}`)
-    throw e
   }
 }
--- source/Background/Functions/requestAndIndexOwnedGames.ts
+++ source/Background/Functions/requestAndIndexOwnedGames.ts
@@ -1,6 +1,6 @@
 import { indexOwnedGames } from '../../lib/db'
 import { Debug, Verbosity } from '../../lib/debug'
-import { StorageKey } from '../../lib/enums'
+import { StorageKey, USE_CACHING } from '../../lib/enums'
 import {
   getFromLocalStorage,
   saveToLocalStorage,
@@ -12,23 +12,45 @@ export async function requestAndIndexOwnedGames(
   apiUrl: string,
 ) {
   Debug.log(Verbosity.INFO, `Requesting owned games from ${apiUrl}`)
-  // TODO refactor
-  let ownedGames
-  const temp = await getFromLocalStorage(StorageKey.OwnedGames)
-  if (temp) {
-    ownedGames = temp.data
+
+  const fromStorage = await getFromLocalStorage(StorageKey.OwnedGames)
+  if (
+    USE_CACHING &&
+    fromStorage &&
+    fromStorage?.data?.response?.games?.length > 0 &&
+    Date.now() - fromStorage.timestamp <= 1000 * 60 * 10
+  ) {
+    Debug.log(
+      Verbosity.INFO,
+      `Retrieved owned games from storage, containing ${
+        fromStorage.data.response.games.length
+      } records, from ${new Date(fromStorage.timestamp).toTimeString()}`,
+    )
+    Debug.log(Verbosity.LOG, 'Cached data is less than 10 minutes old')
   } else {
-    ownedGames = await requestFromSteam(apiUrl, SteamEndpoint.GetOwnedGames, {
-      steamId,
-    })
+    try {
+      Debug.log(
+        Verbosity.LOG,
+        'Cached data is more than 10 minutes old or non-existent, calling API',
+      )
+      const ownedGames = await requestFromSteam(
+        apiUrl,
+        SteamEndpoint.GetOwnedGames,
+        {
+          steamId,
+        },
+      )
 
-    await saveToLocalStorage({
-      [StorageKey.OwnedGames]: {
-        data: ownedGames,
-        timestamp: Date.now(),
-      },
-    })
+      await saveToLocalStorage({
+        [StorageKey.OwnedGames]: {
+          data: ownedGames,
+          timestamp: Date.now(),
+        },
+      })
+
+      await indexOwnedGames(ownedGames.response.games)
+    } catch (e) {
+      Debug.log(Verbosity.WARN, `Failed to update owned games`)
+    }
   }
-
-  await indexOwnedGames(ownedGames.response.games)
 }
