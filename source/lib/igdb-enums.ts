b1b1e79737287efdfceb36f773e510051af10d5a
Aldert Vaandering <aldert.vaandering@digib.com>
2020-09-19 22:31:05 +0300

    Added WebsiteCategory enum

--- source/lib/igdb-enums.ts
+++ source/lib/igdb-enums.ts
@@ -162,3 +162,23 @@ export enum AgeRating {
   M = 11,
   AO = 12,
 }
+
+export enum WebsiteCategory {
+  Official = 1,
+  Wikia = 2,
+  Wikipedia = 3,
+  Facebook = 4,
+  Twitter = 5,
+  Twitch = 6,
+  Instagram = 8,
+  Youtube = 9,
+  Iphone = 10,
+  Ipad = 11,
+  Android = 12,
+  Steam = 13,
+  Reddit = 14,
+  Itch = 15,
+  Epicgames = 16,
+  Gog = 17,
+  Discord = 18,
+}
